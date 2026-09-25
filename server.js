ab1b84530bf1cf45e82935927769bae36047fac1
Umama11-arch <umamaishaq11@gmail.com>
2026-09-25 05:32:01 +0500

    CORS

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const app = express();
 import cors from "cors";
 
 app.use(cors({
-    origin: "https://umama11-arch.github.io"
+    origin: "https://umama11-arch.github.io/SmartRecommend/"
 }));
 
 
